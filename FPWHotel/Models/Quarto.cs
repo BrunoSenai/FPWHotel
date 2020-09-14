07cc2ee936f36e251d35f110caddef0390107cf2
Bruno Komiyama <brunoribeiro123@outlook.com>
2020-09-13 21:37:48 -0300

    Criação do projeto final

---
+++ FPWHotel/Models/Quarto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPWHotel.Models
+{
+    [Table("Quartos")]
+    class Quarto
+    {
+        [Key]
+        public int IdQuarto { get; set; }
+
+        public int NumeroQuarto { get; set; }
+
+        public double PrecoDiaria { get; set; }
+
+        public int NumCamasSolteiro { get; set; }
+
+        public int NumCamasCasal { get; set; }
+
+        public string Descricao { get; set; }
+
+        public Boolean Ocupado { get; set; }
+
+    }
+}
